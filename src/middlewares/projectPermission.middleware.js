13444671edbd69ff274c4a6a5a12c86ad27f282c
mubeenlodhie52 <mubeenlodhie52@gmail.com>
2026-02-09 18:43:04 +0500

    Complete projectPermission middle ware

---
+++ src/middlewares/projectPermission.middleware.js
@@ -0,0 +1,33 @@
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ProjectMember } from "../models/projectMember.models.js";
+import mongoose from "mongoose";
+
+const validateProjectPermission = (roles = []) => {
+    return asyncHandler(async (req,res,next)=>{
+        const {projectId} = req.params;
+
+        if(!projectId){
+            throw new ApiError(400,"Project id is missing!");
+        }
+
+        const projectMember = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        });
+        if(!projectMember){
+            throw new ApiError(404,"Project member not found! ")
+        }
+
+        const givenRole = projectMember?.role
+
+        req.user.role = givenRole;
+
+        if(!roles.includes(givenRole)){
+            throw new ApiError(403,"You dont have permission to perform this task!")
+        }
+
+        next();
+
+    })
+}
